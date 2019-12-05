c129a7d589e3b624476275e77fcda3ca0027842e
prabhjotsingh10 <prabhjot.singh232@gmail.com>
2019-12-05 00:29:21 -0500

    third-commit: added more grids and interface

--- js/index.js
+++ js/index.js
@@ -2,61 +2,66 @@
 function getInfoFromUser() {
 
     /************VARIABLE DECLARATION ***************/
+
     let name = document.getElementById('name').value;
     let number = document.getElementById('number').value;
     let dept = document.getElementById("department").value;
     let code = document.getElementById("emp_code").value;
     let hours = parseInt(document.getElementById("work_hours").value);
-    let gross_sal, hourly_wage, overtime, sal, income_tax, net, health;
+    let gross_sal, hourly_wage, overtime, fixed_sal, income_tax, net, health;
 
     if (code == "F") {
         const qualif_code = window.prompt("Please Enter the Qualification Code");
-        console.log(code);
 
         if (qualif_code == "M") {
-            gross_sal = hours * 175 + 1500; console.log(gross_sal);
+            gross_sal = hours * 175 + 1500; 
             calculate_deductions();
         }
 
         else if (qualif_code == "B") {
             gross_sal = hours * 100 + 600;
-            console.log(gross_sal);
             calculate_deductions();
+        }
+
+        else{
+            alert("Invalid Code! Please try again!");
         }
     }
 
     else if (code == "R") {
-        gross_sal = window.prompt("Please Enter the fixed salary per month");
-        hourly_wage = gross_sal / 160;
+        fixed_sal = window.prompt("Please Enter the fixed salary per month");
+        hourly_wage = fixed_sal / 160;
 
 
         if (hours == 160) {
-            sal = gross_sal;
-            console.log(sal);
+            gross_sal = fixed_sal;
             calculate_deductions();
-            paySlip();
         }
 
         else if (hours < 160) {
             gross_sal = hours * hourly_wage;
-            console.log(gross_sal);
             calculate_deductions();
         }
 
         else {
             overtime = hours - 160;
             gross_sal = (160 * hourly_wage) + overtime * (hourly_wage * 2);
-            console.log(gross_sal);
             calculate_deductions();
         }
     }
 
+    else{
+        alert("Invalid Code! Please try again!");
+    }
+
+    /************NESTED FUNCTIONS *****************/
+
     function calculate_deductions() {
 
-        if (gross_sal > 2500 && gross_sal > 3000) {
-            income_tax = gross_sal * 0.25; console.log(income_tax);
-            health = 33;
-            net = income_tax - health + 2500; console.log(net);
+        if (gross_sal > 3000) {
+            income_tax = (gross_sal - 2500) * 0.25;
+            health = 33; 
+            net = gross_sal - income_tax - health ;
             paySlip();
         }
 
@@ -77,17 +82,22 @@ function getInfoFromUser() {
     }
     
     function paySlip() {
-        console.log("Name:", name, "Employee number ", number);
-        console.log("Employee Department", dept);
-        console.log("Qualification Code", code);
-        console.log("Number of Hours Worked", hours);
-        console.log("Gross salary for the employee", gross_sal);
-        console.log("Deductions for income tax and health subcharge fee respectively : ", income_tax, health);
-        console.log("Total In-hand salary", net);
+
+        document.getElementById("displayed_name").value = name;
+        document.getElementById("displayed_number").value = number;
+        document.getElementById("displayed_qualif_code").value = code;
+        document.getElementById("displayed_department").value = dept;
+        document.getElementById("displayed_work_hours").value = hours;
+        document.getElementById("displayed_gross_sal").value = "$ " + gross_sal;
+        document.getElementById("displayed_income_tax").value = "$ "+ income_tax;
+        document.getElementById("displayed_health_fee").value = "$ " + health;
+        document.getElementById("displayed_total_sal").value = "$ " + net;        
+       
     }
 
 }
 
+/*************EXECUTION *****************/
 
 let btn = document.getElementById("type");
 btn.addEventListener("click", getInfoFromUser);
